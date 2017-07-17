6caed8f67c47ccab92c7707d635bac0ff60a0796
lx <lx364301766@gmail.com>
2017-07-17 20:41:39 +0800

    add support wrap_content for fresco

--- frescobridge/src/main/java/pl/atlantischi/ximagebridge/fresco/FrescoBridge.java
+++ frescobridge/src/main/java/pl/atlantischi/ximagebridge/fresco/FrescoBridge.java
@@ -2,25 +2,28 @@ package pl.atlantischi.ximagebridge.fresco;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
-import com.facebook.drawee.controller.AbstractDraweeControllerBuilder;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.drawee.controller.ControllerListener;
+import com.facebook.drawee.generic.GenericDraweeHierarchy;
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
 import com.facebook.drawee.generic.RoundingParams;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.common.ResizeOptions;
-import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.image.ImageInfo;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
-import com.facebook.imagepipeline.request.Postprocessor;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Color;
+import android.graphics.drawable.Animatable;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v4.view.LayoutInflaterFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.ViewGroup;
 import pl.atlantischi.ximagebridge.XImageBridge;
 import pl.atlantischi.ximagebridge.fresco.processors.BlurPostprocessor;
 import pl.atlantischi.ximagebridge.interfaces.IFrescoBridge;
@@ -64,7 +67,7 @@ public class FrescoBridge implements IFrescoBridge<SimpleDraweeView> {
     }
 
     @Override
-    public void display(Uri uri, SimpleDraweeView draweeView, XImageBridge.Options options) {
+    public void display(Uri uri, final SimpleDraweeView draweeView, XImageBridge.Options options) {
 //        if (imageView instanceof SimpleDraweeView) {
 //            SimpleDraweeView draweeView = (SimpleDraweeView) imageView;
 //            draweeView.setImageURI(uri);
@@ -72,6 +75,7 @@ public class FrescoBridge implements IFrescoBridge<SimpleDraweeView> {
 
         Context context = draweeView.getContext();
 
+        GenericDraweeHierarchy draweeHierarchy = new GenericDraweeHierarchyBuilder(context.getResources()).build();
         ImageRequestBuilder imageRequestBuilder = ImageRequestBuilder.newBuilderWithSource(uri);
         if (options != null) {
             RoundingParams roundingParams = new RoundingParams();
@@ -81,7 +85,7 @@ public class FrescoBridge implements IFrescoBridge<SimpleDraweeView> {
             if (options.roundCorner > 0) {
                 roundingParams.setCornersRadius(options.roundCorner);
             }
-            draweeView.getHierarchy().setRoundingParams(roundingParams);
+            draweeHierarchy.setRoundingParams(roundingParams);
 
             if (options.blurRadius > 0) {
                 imageRequestBuilder.setPostprocessor(new BlurPostprocessor(context, options.blurRadius));
@@ -92,8 +96,33 @@ public class FrescoBridge implements IFrescoBridge<SimpleDraweeView> {
                 imageRequestBuilder.setResizeOptions(new ResizeOptions(imageWidth, imageHeight));
             }
         }
+
+        ControllerListener controllerListener = new BaseControllerListener<ImageInfo>() {
+
+            @Override
+            public void onIntermediateImageSet(String id, @Nullable ImageInfo imageInfo) {
+                updateViewSize(draweeView, imageInfo);
+            }
+
+            @Override
+            public void onFinalImageSet(String id, @Nullable ImageInfo imageInfo, @Nullable Animatable animatable) {
+                updateViewSize(draweeView, imageInfo);
+            }
+
+            void updateViewSize(SimpleDraweeView draweeView, @Nullable ImageInfo imageInfo) {
+                if (imageInfo != null) {
+                    draweeView.getLayoutParams().width = imageInfo.getWidth();
+                    draweeView.getLayoutParams().height = ViewGroup.LayoutParams.WRAP_CONTENT;
+                    draweeView.setAspectRatio((float) imageInfo.getWidth() / imageInfo.getHeight());
+                }
+            }
+
+        };
+
+        draweeView.setHierarchy(draweeHierarchy);
         PipelineDraweeController controller = (PipelineDraweeController) Fresco.newDraweeControllerBuilder()
                 .setImageRequest(imageRequestBuilder.build())
+                .setControllerListener(controllerListener)
                 .setOldController(draweeView.getController())
                 .build();
         draweeView.setController(controller);
--- simple/src/main/java/pl/atlantischi/ximagebridge/simple/SimpleMainActivity.java
+++ simple/src/main/java/pl/atlantischi/ximagebridge/simple/SimpleMainActivity.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import pl.atlantischi.ximagebridge.XImageBridge;
+import timber.log.Timber;
 
 public class SimpleMainActivity extends AppCompatActivity {
 
     String url = "http://e.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=75aaa91fa444ad342eea8f83e59220c2"
             + "/0bd162d9f2d3572cf556972e8f13632763d0c388.jpg";
+
+    String webP = "https://p.upyun.com/demo/webp/webp/animated-gif-0.webp";
+
+    String gif = "https://p.upyun.com/demo/webp/animated-gif/0.gif";
 
 //    @Override
 //    public View onCreateView(String name, Context context, AttributeSet attrs) {
@@ -22,19 +27,19 @@ public class SimpleMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         XImageBridge.obtain().compatFresco(this);
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ximage_compat);
-
         setTitle(XImageBridge.obtain().getBridge().getClass().getSimpleName());
+        setContentView(R.layout.activity_ximage_compat);
 
         final ImageView iv = (ImageView) findViewById(R.id.imageView);
         XImageBridge.Options options = new XImageBridge.Options();
 //        options.isCircle = true;
         options.roundCorner = 50;
 //        options.blurRadius = 10;
-        options.size = new int[] {600, 400};
+        options.size = new int[] {300, 200};
         XImageBridge.obtain().display(Uri.parse(url), iv, options);
 
 //        mXImageBridge.getBitmapFromUri(Uri.parse(url), new ImageBridge.BitmapLoader() {

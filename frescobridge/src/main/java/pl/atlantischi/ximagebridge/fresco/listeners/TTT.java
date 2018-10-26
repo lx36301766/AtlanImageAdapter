309884f85547cbf5f41d25ac9099b89bbd8dd035
lx364301766@gmail.com <lx364301766@gmail.com>
2018-10-27 00:11:11 +0800

    upgrade gradle version

--- frescobridge/src/main/java/pl/atlantischi/ximagebridge/fresco/listeners/TTT.java
+++
@@ -1,9 +0,0 @@
-package pl.atlantischi.ximagebridge.fresco.listeners;
-
-/**
- * Created by admin on 2017/7/17.
- */
-
-public class TTT  <BUILDER extends TTT<BUILDER, REQUEST, IMAGE, INFO>, REQUEST, IMAGE, INFO> {
-
-}
